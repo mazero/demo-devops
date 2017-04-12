c05e76a676b843f5158839cfa4dcec0e10fab854
weiqi <happybird.w@gmail.com>
2017-04-12 10:06:23 +0200

    step mlab ok

--- environments/production.ts
+++ environments/production.ts
@@ -8,6 +8,6 @@ export const prodVariables:IEnvironment = {
   apiEndpoint: 'http://localhost:8080',
 
   // Back-end
-  dbHost: 'mongodb://localhost:27017',
-  dbName: 'test'
+  dbHost: 'mongodb://ionic-devops:4353453@ds135820.mlab.com:35820',
+  dbName: 'ionic-devops'
 };
