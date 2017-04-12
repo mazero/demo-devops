1a122bc26de2e7c48708b2d3c0b770ba3671a3ff
weiqi <happybird.w@gmail.com>
2017-04-12 11:03:47 +0200

    add folder dist

---
+++ dist/environments/development.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.devVariables = {
+    environmentName: 'Development Environment',
+    ionicEnvName: 'dev',
+    // Front-end
+    apiEndpoint: 'http://localhost:8080',
+    // Back-end
+    dbHost: 'mongodb://localhost:27017',
+    dbName: 'test'
+};
---
+++ dist/environments/production.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.prodVariables = {
+    environmentName: 'Production Environment',
+    ionicEnvName: 'prod',
+    // Front-end
+    apiEndpoint: 'http://localhost:8080',
+    // Back-end
+    dbHost: 'mongodb://ionic-devops:4353453@ds135820.mlab.com:35820',
+    dbName: 'ionic-devops'
+};
